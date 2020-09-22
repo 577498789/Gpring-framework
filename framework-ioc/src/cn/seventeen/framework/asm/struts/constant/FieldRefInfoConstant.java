9561c53f000f4cac370f8c0749533572f30e37a0
577498789 <577498789@qq.com>
2020-09-22 10:59:49 +0800

    修改字段的类结构描述

--- framework-ioc/src/cn/seventeen/framework/asm/struts/constant/FieldRefInfoConstant.java
+++ framework-ioc/src/cn/seventeen/framework/asm/struts/constant/FieldRefInfoConstant.java
@@ -1,15 +1,19 @@
 package cn.seventeen.framework.asm.struts.constant;
 
+/**
+ * 该类是类文件结构中，常量表的其中之一，
+ * 字段引用的常量，
+ * 其实际值是对于常量表的引用，他引用了常量表中的具体的常量，
+ * 那个常量的值，就是该字段的实际引用类
+ */
 public class FieldRefInfoConstant implements ConstantStruts{
 
     private byte[] fieldRef;
 
-    @Override
     public byte[] getValue() {
         return fieldRef;
     }
 
-    @Override
     public void setValue(byte[] value) {
         this.fieldRef = value;
     }
