e5ffad45f88fe0184dd41c79de541c01d2c9ea90
577498789 <577498789@qq.com>
2020-09-28 17:54:38 +0800

    14个常量结构类添加完成

---
+++ framework-ioc/src/cn/seventeen/framework/asm/struts/FieldStrutsConstant.java
@@ -0,0 +1,24 @@
+package cn.seventeen.framework.asm.struts;
+
+public interface FieldStrutsConstant {
+    /**
+     * 以下是字段访问标志的常量
+     */
+
+    // 访问权限public
+    public static final int ACC_PUBLIC = 0x0001;
+
+    // 访问权限private
+    public static final int ACC_PRIVATE = 0x0002;
+
+    // 访问权限protected
+    public static final int ACC_PROTECTED = 0x0004;
+
+    // 访问权限静态
+    public static final int ACC_STATIC = 0x0008;
+
+    // 访问权限final
+    public static final int ACC_FINAL = 0x0100;
+
+
+}
