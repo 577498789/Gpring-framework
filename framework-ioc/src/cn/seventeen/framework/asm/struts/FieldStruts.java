09e4f83869281820ffa1d15b903faebffa4adaca
577498789 <577498789@qq.com>
2020-09-28 18:06:13 +0800

    14个常量结构类添加完成

--- framework-ioc/src/cn/seventeen/framework/asm/struts/FieldStruts.java
+++ framework-ioc/src/cn/seventeen/framework/asm/struts/FieldStruts.java
@@ -1,4 +1,18 @@
 package cn.seventeen.framework.asm.struts;
 
+/**
+ * 字段表，
+ * 用于存储类中的所有成员变量
+ */
 public class FieldStruts {
+
+    // 字段修饰符
+    public int ACCESS_FLAGS;
+
+    // 名称的常量引用
+    public int nameIndex;
+
+    // 字段描述符的引用
+    public int infoIndex;
+
 }
