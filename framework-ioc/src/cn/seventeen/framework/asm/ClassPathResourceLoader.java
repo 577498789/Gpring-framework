3ce4be43b346035bd29eeb8fc12f2153d5dcbe06
577498789 <577498789@qq.com>
2020-10-13 21:59:40 +0800

    修改类路径资源加载器中 将资源文件转化问byte数组的方法

--- framework-ioc/src/cn/seventeen/framework/asm/ClassPathResourceLoader.java
+++ framework-ioc/src/cn/seventeen/framework/asm/ClassPathResourceLoader.java
@@ -8,22 +8,29 @@ import java.io.InputStream;
 public class ClassPathResourceLoader {
 
     // 通过类名获取类的元数据
-    public char[] getMateDataForClass(String className) throws Exception {
+    public byte[] getMateDataForClass(String className) throws Exception {
         VariableCharArray variableArray = new VariableCharArray();
 
         if(className == null|| className.length() ==0){
             throw new Exception("待转化为汇编码的类名为空......");
         }
-        char [] chars = new char[1024];
+
         InputStream resourceAsStream = this.getClass().getClassLoader().getResourceAsStream(className.replace('.', '/'));
-        int code = -1;
-        while (-1!=(code = resourceAsStream.read())){
-            chars[code] = (char) code;
+        int dataSize = 0;
+        while (-1!=resourceAsStream.read()){
+            dataSize++;
         }
-        return chars;
+        byte [] bytes = new byte[dataSize];
+        int code = -1;
+        int index = 0;
+        resourceAsStream.reset();
+        while (-1!=(code = resourceAsStream.read())){
+            bytes[index++] = (byte) code;
+        }
+        return bytes;
     }
 
-    public char[] getMateDataForClass(Class clazz) throws Exception {
+    public byte[] getMateDataForClass(Class clazz) throws Exception {
         return getMateDataForClass(clazz.toString());
     }
 
